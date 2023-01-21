6e5f7611fa797dcd3559550203f6abe30ac1bb57
eles <eleswastaken@gmail.com>
2023-01-21 08:22:56 +0300

    2

--- app.js
+++ app.js
@@ -1,49 +1,11 @@
-
-if (process.env.NODE_ENV == "DEVELOPMENT") {
-  require('dotenv').config()
-}
-const express = require('express');
-const mongoose = require('mongoose');
-const cookieParser = require('cookie-parser');
-
-const postsRouter = require('./routers/posts');
-const adminRouter = require('./routers/admin');
-
-function connectMongoDB() {
-
-  const mongoDB = process.env.MONGODB_URI;
-  mongoose.set('strictQuery', false)
-  mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
-
-  const db = mongoose.connection;
-
-  db.on('open', () => console.log('Connected to MongoDB'))
-
-  db.on("error", console.error.bind(console, "MongoDB connection error:")); 
-}
-
-connectMongoDB()
-
-
+const express = require('express')
 const app = express()
-
-app.set('view engine', 'ejs');
-app.use(express.json());
-app.use(cookieParser());
-
-app.use('/posts', postsRouter);
-app.use('/admin', adminRouter);
+const port = 3000
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-
-
-const PORT = process.env.PORT || 5000
-
-app.listen(PORT, () => {
-  console.log('App is running...')
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
 })
-
-module.exports = app;
