aef031a6c09daac567ae29e9c762c2d79b2be79b
eles <eleswastaken@gmail.com>
2023-01-21 14:50:25 +0300

    Create nextjs project

---
+++ pages/posts/[slug].js
@@ -0,0 +1,13 @@
+
+import { useRouter } from 'next/router'
+
+export default function Post() {
+    const router = useRouter();
+    const { slug } = router.query;
+
+    return (
+        <>
+            <p>{ slug }</p>
+        </>
+    )
+}
