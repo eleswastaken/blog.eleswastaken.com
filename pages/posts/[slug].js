fe1eff6a83fa344fad24b5eaca915da975874fcf
eles <eleswastaken@gmail.com>
2023-01-23 20:22:46 +0300

    Style post slug page

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -3,14 +3,27 @@ import markdownIt from 'markdown-it';
 import PostModel from '@/models/Post';
 
 export default function Post({ p }) {
-    return (
-        <>
-            <h1>{ p.title }</h1>
-            <article dangerouslySetInnerHTML={{__html: (p.html)}}>
-            </article>
-
-        </>
-    )
+  const date = new Date(p.createdAt)
+  const formatted = date.toLocaleDateString('en-US', {year: "numeric", month: "long", day:"numeric"})
+  return (
+      <>
+        <article className='blog-post'>
+          <time dateTime={date}>{ formatted }</time>
+          <div className="meta-data">
+            <div className="category">
+              My Life
+            </div>
+            <hr />
+            <div className="writer">
+              Eles Ismailov
+            </div>
+          </div>
+          <h1 className='title'>{ p.title }</h1>
+          <hr className='main-hr'/>
+          <div className="wrapper" dangerouslySetInnerHTML={{__html: (p.html)}}></div>
+        </article>
+      </>
+  )
 }
 
 export async function getServerSideProps({ params }) {
