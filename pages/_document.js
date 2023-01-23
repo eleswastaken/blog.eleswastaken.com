4a45d588e5351451261a4b9da989ecfb44b0b3df
eles <eleswastaken@gmail.com>
2023-01-23 10:13:35 +0300

    Add google analytics

--- pages/_document.js
+++ pages/_document.js
@@ -1,12 +1,14 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import GoogleAnalytics from './googleAnalytics'
 
 export default function Document() {
   return (
     <Html lang="en">
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous"/>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous"/>
 
-
-      <Head />
+      <Head>
+        <GoogleAnalytics />
+      </Head>
       <body>
         <Main />
         <NextScript />
---
+++ pages/googleAnalytics.js
@@ -0,0 +1,9 @@
+
+export default function GoogleAnalytics() {
+  return (
+    <>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-5FJST3Z11R"></script>
+        <script defer src="/googleAnalytics.js"/>
+    </>
+  )
+}
