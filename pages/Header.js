4550de5ecd8473fa2465ddc10a993cd6dcc770d6
eles <eleswastaken@gmail.com>
2023-02-04 16:09:12 +0300

    Style header

--- pages/Header.js
+++ pages/Header.js
@@ -11,20 +11,24 @@ export default function Header() {
                       </div>
                     </a>
                 </div>
+
+                <button className="mobile-menu">
+                  M
+                </button>
                 <div className="nav-wrapper">
-                    <nav>
+                    <nav className='nav-links'>
                         <ul>
                             <li><a href="/blog">Blog</a></li>
                             <li><a href="/contact">Contact</a></li>
                             <li><a href="/about">About</a></li>
                         </ul>
                     </nav>
-                    <div className="social-media">
+                    <nav className="social-media">
                         <ul>
                             <li><a target="_blank" href="https://twitter.com/elesismailov">Twitter</a></li>
                             <li><a target="_blank" href="https://www.youtube.com/@elesismailov/">YouTube</a></li>
                         </ul>
-                    </div>
+                    </nav>
                 </div>
             </div>
         </header>
